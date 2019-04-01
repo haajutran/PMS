17ad5f6d5a6c52fbf322cd0161c99cc88430a09a
Hau <haajutran@outlook.com>
2019-04-01 21:06:27 +0700

    3

--- PMS/ClientApp/src/components/Reservation.js
+++ PMS/ClientApp/src/components/Reservation.js
@@ -10,6 +10,7 @@ import {
   Input,
   Icon,
   Button,
+  Checkbox,
   Radio,
   Select,
   Switch,
@@ -23,6 +24,9 @@ const { TextArea } = Input;
 class Reservation extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      checkNick: false
+    };
   }
 
   componentDidMount() {}
@@ -194,11 +198,22 @@ class Reservation extends Component {
                 <Form.Item {...formItemLayout} label="Res.Notice">
                   {getFieldDecorator("resNotice", {
                     rules: []
-                  })(<TextArea placeholder="Res.Notice" />)}
+                  })(<TextArea placeholder="Res.Notice" rows={19} />)}
+                </Form.Item>
+                <Form.Item {...formItemLayout} label="Market Segment">
+                  {getFieldDecorator("merketSegment", {
+                    rules: [{}]
+                  })(<Input placeholder="Market Segment" />)}
+                </Form.Item>
+                <Form.Item {...formItemLayout} label="Abc">
+                  {getFieldDecorator("abc", {
+                    rules: [{}]
+                  })(<Input placeholder="Abc" />)}
                 </Form.Item>
               </div>
             </Col>
-            <Col md={24} lg={12}>
+
+            <Col md={24} xl={12}>
               <div className="reservation-information">
                 <h2>Revervation Information</h2>
 
@@ -230,12 +245,7 @@ class Reservation extends Component {
                 </Form.Item>
                 <Form.Item {...formItemLayout} label="Allotment">
                   {getFieldDecorator("allotment", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "Please select arrival date!"
-                      }
-                    ]
+                    rules: []
                   })(<Input placeholder="Allotment" />)}
                 </Form.Item>
                 <Form.Item {...formItemLayout} label="Rate of type">
@@ -253,6 +263,413 @@ class Reservation extends Component {
                     </Select>
                   )}
                 </Form.Item>
+                <Form.Item
+                  {...formItemLayout}
+                  label="Guest Type"
+                  className="guest-type"
+                >
+                  <Row>
+                    <Col span={8}>Adult</Col>
+                    <Col span={8}>Child</Col>
+                    <Col span={8}>Baby</Col>
+                  </Row>
+                </Form.Item>
+                <Form.Item
+                  {...formItemLayout}
+                  label="Num Guest"
+                  style={{ marginBottom: 0 }}
+                >
+                  <Row>
+                    <Col span={8}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("guestTypeAdult", {
+                          rules: []
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("guestTypeChild", {
+                          rules: []
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("guestTypeBoby", {
+                          rules: []
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Form.Item>
+                <Form.Item {...formItemLayout} label="Room Type Book">
+                  {getFieldDecorator("roomTypeBook", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "Please select room type book!"
+                      }
+                    ]
+                  })(<Input placeholder="Room Type Book" />)}
+                </Form.Item>
+                <Form.Item {...formItemLayout} label="Room Type Stay">
+                  {getFieldDecorator("roomTypeStay", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "Please select room type stay!"
+                      }
+                    ]
+                  })(<Input placeholder="Room Type Stay" />)}
+                </Form.Item>
+                <Form.Item {...formItemLayout} label="Room No">
+                  {getFieldDecorator("roomNo", {
+                    rules: [{}]
+                  })(<Input placeholder="Room No" />)}
+                </Form.Item>
+
+                <Form.Item
+                  {...formItemLayout}
+                  label="Rate Code"
+                  style={{ marginBottom: 0 }}
+                >
+                  <Row>
+                    <Col span={8}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("rateCode", {
+                          rules: [
+                            {
+                              required: true,
+                              message: "Please input rate code!"
+                            }
+                          ]
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("currency", {
+                          rules: []
+                        })(
+                          <Select
+                            value={"VNĐ"}
+                            style={{ width: "100%" }}
+                            // onChange={this.handleCurrencyChange}
+                          >
+                            <Option value="VNĐ">VNĐ</Option>
+                            <Option value="Dollar">Dollar</Option>
+                          </Select>
+                        )}
+                      </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("title", {
+                          rules: []
+                        })(<Input />)}
+                      </Form.Item>
+                    </Col>
+                    <Col span={7} offset={1}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("nicknameRequired", {
+                          rules: []
+                        })(
+                          <Checkbox
+                            checked={this.state.checkNick}
+                            onChange={this.handleChangeNicknameRequired}
+                          >
+                            Nickname is required
+                          </Checkbox>
+                        )}
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Form.Item>
+                <Form.Item
+                  {...formItemLayout}
+                  label="Rate Amount"
+                  style={{ marginBottom: 0 }}
+                >
+                  <Row>
+                    <Col span={19}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("rateCode", {
+                          rules: [
+                            {
+                              required: true,
+                              message: "Please input rate code!"
+                            }
+                          ]
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+
+                    <Col span={4} offset={1}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("nett", {
+                          rules: []
+                        })(
+                          <Checkbox
+                            checked={this.state.checkNick}
+                            onChange={this.handleChangeNicknameRequired}
+                          >
+                            NETT
+                          </Checkbox>
+                        )}
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Form.Item>
+
+                <Form.Item
+                  {...formItemLayout}
+                  label="Discount"
+                  style={{ marginBottom: 0 }}
+                >
+                  <Row>
+                    <Col span={20}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("discount", {
+                          rules: []
+                        })(
+                          <InputNumber
+                            style={{
+                              width: "100%"
+                            }}
+                            min={0}
+                            formatter={value => `${value}%`}
+                            parser={value => value.replace("%", "")}
+                            onChange={() => this.onChange}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+
+                    <Col span={4}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("currency", {
+                          rules: []
+                        })(<Input />)}
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Form.Item>
+                <Form.Item {...formItemLayout} label="Discount Reason">
+                  {getFieldDecorator("discountReason", {
+                    rules: [{}]
+                  })(<Input placeholder="Discount Reason" />)}
+                </Form.Item>
+                <Form.Item {...formItemLayout} label="Package Code">
+                  {getFieldDecorator("packageCode", {
+                    rules: [{}]
+                  })(<Input placeholder="Package Code" />)}
+                </Form.Item>
+
+                <Form.Item {...formItemLayout} label="Start Date">
+                  {getFieldDecorator("startDate", {})(
+                    <DatePicker format="DD-MM-YYYY" />
+                  )}
+                </Form.Item>
+
+                <Form.Item
+                  {...formItemLayout}
+                  label="Meal Code"
+                  style={{ marginBottom: 0 }}
+                >
+                  <Row>
+                    <Col span={12}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("mealCode", {
+                          rules: []
+                        })(<Input />)}
+                      </Form.Item>
+                    </Col>
+
+                    <Col span={4}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("mealCode1", {
+                          rules: []
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("mealCode2", {
+                          rules: []
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                      <Form.Item
+                        style={{
+                          display: "inline-block",
+                          width: "100%"
+                        }}
+                      >
+                        {getFieldDecorator("mealCode3", {
+                          rules: []
+                        })(
+                          <InputNumber
+                            min={0}
+                            style={{
+                              width: "100%"
+                            }}
+                          />
+                        )}
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Form.Item>
+
+                <Form.Item {...formItemLayout} label="Payment Method">
+                  {getFieldDecorator("paymentMethod", {
+                    rules: [{}]
+                  })(<Input placeholder="Payment Method" />)}
+                </Form.Item>
+
+                <Form.Item {...formItemLayout} label="Card Number">
+                  {getFieldDecorator("cardNumber", {
+                    rules: [{}]
+                  })(<Input placeholder="Card Number" />)}
+                </Form.Item>
+
+                <Form.Item {...formItemLayout} label="Card Exp Date">
+                  {getFieldDecorator("cardExpDate", {})(
+                    <DatePicker format="DD-MM-YYYY" />
+                  )}
+                </Form.Item>
+
+                <Form.Item {...formItemLayout} label="Number On Card">
+                  {getFieldDecorator("numberOnCard", {
+                    rules: [{}]
+                  })(<Input placeholder="Number On Card" />)}
+                </Form.Item>
+
+                <Form.Item {...formItemLayout} label="CVV">
+                  {getFieldDecorator("cvv", {
+                    rules: []
+                  })(<TextArea placeholder="CVV" rows={3} />)}
+                </Form.Item>
               </div>
             </Col>
           </Row>
@@ -260,6 +677,19 @@ class Reservation extends Component {
       </div>
     );
   }
+  handleChangeNicknameRequired = e => {
+    this.setState(
+      {
+        checkNick: e.target.checked
+      },
+      () => {
+        this.props.form.validateFields(["nickname"], { force: true });
+      }
+    );
+  };
+  onChange = value => {
+    console.log("changed", value);
+  };
 }
 
 const MainComponent = Form.create()(Reservation);
